1082bd7aedf4c68ec2525c40e9f6a863a86b753c
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-03-19 23:06:47 +0100

    fix(planeta): Agregar venus a los planetas

--- src/main/java/edu/poniperro/arnold/Planeta.java
+++ src/main/java/edu/poniperro/arnold/Planeta.java
@@ -2,6 +2,7 @@ package edu.poniperro.arnold;
 
 public enum Planeta {
     MERCURY(3.303e+23, 2.4397e6),
+    VENUS (4.869e+24, 6.0518e6),
     EARTH(5.976e+24, 6.37814e6),
     MARS(6.421e+23, 3.3972e6),
     JUPITER(1.9e+27, 7.1492e7),
