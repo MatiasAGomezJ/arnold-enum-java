521b573b08df1491ad9739a8ad8a6be1db18bcb9
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-03-19 19:53:13 +0100

    First Commit, only tests

---
+++ src/main/java/edu/poniperro/arnold/Main.java
@@ -0,0 +1,13 @@
+package edu.poniperro.arnold;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
