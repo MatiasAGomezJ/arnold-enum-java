bd0774ef7807cceafe61b7d06d73457d7c22650a
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-03-19 23:26:30 +0100

    feat(planeta): Devolver EnumSet con todos los planetas terrestres

--- src/main/java/edu/poniperro/arnold/Planeta.java
+++ src/main/java/edu/poniperro/arnold/Planeta.java
@@ -1,8 +1,10 @@
 package edu.poniperro.arnold;
 
+import java.util.EnumSet;
+
 public enum Planeta {
     MERCURY(3.303e+23, 2.4397e6),
-    VENUS (4.869e+24, 6.0518e6),
+    VENUS(4.869e+24, 6.0518e6),
     EARTH(5.976e+24, 6.37814e6),
     MARS(6.421e+23, 3.3972e6),
     JUPITER(1.9e+27, 7.1492e7),
@@ -37,5 +39,9 @@ public enum Planeta {
 
     public double pesoSuperficie(double pesoHumano) {
         return getMasaFromPesoTerrestre(pesoHumano) * gravedadEnSuperficie();
+    }
+
+    public static EnumSet<Planeta> getPlanetasTerrestres() {
+        return EnumSet.range(Planeta.MERCURY, Planeta.MARS);
     }
 }
