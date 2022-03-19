0ef008f202f3a5a0983e40f7d30ff72560dc205a
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-03-19 23:04:28 +0100

    fix(planeta): No elevando al cuadrado el radio

--- src/main/java/edu/poniperro/arnold/Planeta.java
+++ src/main/java/edu/poniperro/arnold/Planeta.java
@@ -1,13 +1,13 @@
 package edu.poniperro.arnold;
 
 public enum Planeta {
-    MERCURY (3.303e+23, 2.4397e6),
-    EARTH (5.976e+24, 6.37814e6),
-    MARS (6.421e+23, 3.3972e6),
-    JUPITER (1.9e+27, 7.1492e7),
-    SATURN (5.688e+26, 6.0268e7),
-    URANUS (8.686e+25, 2.5559e7),
-    NEPTUNE (1.024e+26, 2.4746e7);
+    MERCURY(3.303e+23, 2.4397e6),
+    EARTH(5.976e+24, 6.37814e6),
+    MARS(6.421e+23, 3.3972e6),
+    JUPITER(1.9e+27, 7.1492e7),
+    SATURN(5.688e+26, 6.0268e7),
+    URANUS(8.686e+25, 2.5559e7),
+    NEPTUNE(1.024e+26, 2.4746e7);
 
     private double masa;
     private double radio;
@@ -18,16 +18,16 @@ public enum Planeta {
         this.radio = radio;
     }
 
-    public double getMasa(){
+    public double getMasa() {
         return masa;
     }
 
-    public double getRadio(){
+    public double getRadio() {
         return radio;
     }
 
     private double gravedadEnSuperficie() {
-        return G * getMasa() / getRadio();
+        return G * getMasa() / (getRadio() * getRadio());
     }
 
     private double getMasaFromPesoTerrestre(double pesoHumano) {
